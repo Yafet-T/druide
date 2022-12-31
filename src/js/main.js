08c75ca5107b6f545a999b062524ea0c1faa2b3a
Yafet-T <98484743+Yafet-T@users.noreply.github.com>
2022-12-31 23:07:04 +0100

    fini mon bouton pour la navigation

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,9 @@ var buttonTwo = document.querySelector(".button-two");
 var buttonTree = document.querySelector(".button-tree");
 var buttonFour = document.querySelector(".button-four");
 var menuVisible = document.querySelector(".menu-visible");
+var menuOverlay = document.querySelector(".menu-overlay");
+var menu = document.querySelector(".menu");
+var close = document.querySelector(".menu-overlay-close");
 
 menuHover = function () {
   buttonOne.classList.toggle("button-one-act");
@@ -14,9 +17,6 @@ menuHover = function () {
 
 menu.addEventListener("click", menuHover);
 
-var menuOverlay = document.querySelector(".menu-overlay");
-var menu = document.querySelector(".menu");
-
 var openMenu = function () {
   menuOverlay.classList.toggle("is-active");
   menu.classList.toggle("is-open");
@@ -24,3 +24,20 @@ var openMenu = function () {
 
 menu.addEventListener("click", openMenu);
 menuOverlay.addEventListener("click", openMenu);
+
+/*-------------------------------------------------------------------------------------------
+button-haut
+-------------------------------------------------------------------------------------------*/
+
+var move = document.querySelector(".button-move");
+var cercle = document.querySelector(".cercle");
+var ligne = document.querySelector(".ligne-arrow");
+var arrow = document.querySelector(".arrow-right");
+
+on = function () {
+  cercle.classList.toggle("cercle-active");
+  ligne.classList.toggle("ligne-arrow-active");
+  arrow.classList.toggle("arrow-active");
+};
+
+move.addEventListener("click", on);
